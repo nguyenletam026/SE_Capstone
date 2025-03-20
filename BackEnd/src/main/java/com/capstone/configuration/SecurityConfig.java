9ff92e13b53219d99b685bbc64d8b5ee1dc1c220
nguyenletam026 <tamnlse170346@fpt.edu.vn>
2025-03-20 15:52:17 +0700

    Update SecurityConfig.java

--- BackEnd/src/main/java/com/capstone/configuration/SecurityConfig.java
+++ BackEnd/src/main/java/com/capstone/configuration/SecurityConfig.java
@@ -63,7 +63,8 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173")); // Allow frontend origin
+        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000"));// Allow frontend origin
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")); // Allowed methods
         configuration.setAllowedHeaders(List.of("*")); // Allow all headers
         configuration.setAllowCredentials(true);
